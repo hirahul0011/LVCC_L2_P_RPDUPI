f24f7380489c509e094d426dbeb78ada392a772d
hirahul0011 <ra_10001@yahoo.co.in>
2023-09-01 22:28:22 +0530

    Created a servlet that is taking the product ID and using JDBC to query the database for the product

---
+++ src/main/java/ProductDetails.java
@@ -0,0 +1,78 @@
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ecommerce.DBConnection;
+
+/**
+ * Servlet implementation class ProductDetails
+ */
+public class ProductDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		int productID=Integer.parseInt(request.getParameter("productID"));
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("<html><body>");
+			
+			  InputStream in = getServletContext().getResourceAsStream("/WEB-INF/config.properties");
+			  Properties props = new Properties();
+			  props.load(in);
+			  
+			  
+			  DBConnection conn = new DBConnection(props.getProperty("url"), props.getProperty("userid"), props.getProperty("password"));
+			  Statement stmt = conn.getConnection().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			  
+			  ResultSet rst;
+			      		      
+			  rst = stmt.executeQuery("select * from eproduct where ID="+productID);
+			  
+			  out.println("Details of the Product are:<br>");
+			  while (rst.next()) {
+				  out.println("Product ID: " + rst.getInt("ID") + "<BR>Product Name: " + rst.getString("name") +
+						  "<BR>Product Price: " + rst.getBigDecimal("price") +
+						  "<BR>Product Added On: " +rst.getDate("date_added") + "<Br>");
+			  }
+		} catch (ClassNotFoundException | IOException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
